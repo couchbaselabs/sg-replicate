3e549716112847caf46a0245760e92c975e65130
Traun Leyden <traun@couchbase.com>
2014-03-17 09:28:51 -0700

    add fakehttp

--- synctube_test.go
+++ synctube_test.go
@@ -2,12 +2,52 @@ package synctube
 
 import (
 	"github.com/couchbaselabs/go.assert"
+	"github.com/tleyden/fakehttp"
 	"testing"
 )
 
-func TestOneShotReplication(t *testing.T) {
+func TestOneShotReplicationBrokenLocalDoc(t *testing.T) {
 
-	// TODO: setup fakehttp
+	// the simulated sync gateway source only returns a _local doc
+	// with a checkpoint.  after that, the request to the _changes
+	// feed returns invalid responses and the replication
+	// stops and goes into an error state
+
+	// startup fake target server
+	targetServer := fakehttp.NewHTTPServer(5984)
+	targetServer.Start()
+
+	// setup fake response on target server
+	headers := map[string]string{"Content-Type": "application/json"}
+	targetServer.Response(200, headers, "{\"bogus\": true}")
+
+	params := ReplicationParameters{}
+	params.Source = "foo.com"
+	params.Target = targetServer.URL
+	params.Continuous = false
+
+	eventChan := make(chan ReplicationEvent)
+
+	replication := NewReplication(params, eventChan)
+	replication.Start()
+
+	sawReplicationActive := false
+	for replicationEvent := range eventChan {
+		switch replicationEvent.Status() {
+		case ReplicationEvent.STATUS_ACTIVE:
+			sawReplicationActive = true
+		}
+	}
+
+	assert.True(t, sawReplicationActive)
+
+}
+
+func TestOneShotHappyPathReplication(t *testing.T) {
+
+	// Happy Path test -- both the simulated source and targets
+	// do exactly what is expected of them.  Make sure One Shot replication
+	// completes.
 
 	params := ReplicationParameters{}
 	params.Source = "foo.com"
@@ -19,23 +59,27 @@ func TestOneShotReplication(t *testing.T) {
 	replication := NewReplication(params, eventChan)
 	replication.Start()
 
-	sawReplicationStart := false
-	sawReplicationFinish := false
+	sawReplicationActive := false
+	sawReplicationStopped := false
 	for replicationEvent := range eventChan {
 		switch replicationEvent.Status() {
 		case ReplicationEvent.STATUS_ACTIVE:
-			sawReplicationStart = true
+			sawReplicationActive = true
 		case ReplicationEvent.STATUS_STOPPED:
-			sawReplicationStart = true
+			sawReplicationStopped = true
 			// TODO: assertions about num documents transferred
 		}
 	}
 
-	assert.True(t, sawReplicationStart)
-	assert.True(t, sawReplicationFinish)
+	assert.True(t, sawReplicationActive)
+	assert.True(t, sawReplicationStopped)
 
 }
 
-func TestContinuousReplication(t *testing.T) {
+func TestContinuousHappyPathReplication(t *testing.T) {
+
+	// Happy Path test -- both the simulated source and targets
+	// do exactly what is expected of them.  Make sure Continous replication
+	// completes.
 
 }
