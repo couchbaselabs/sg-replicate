6ad122a98733ec0bd9c6defae9c71dc36d59c60c
Ben Brooks <ben.brooks@couchbase.com>
2019-06-13 17:37:12 +0100

    Use atomic operations instead of mutex (#66)

--- replication_stats.go
+++ replication_stats.go
@@ -1,9 +1,11 @@
 package sgreplicate
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 type ReplicationStats struct {
-	lock                       sync.RWMutex
 	docsRead                   uint32
 	docsWritten                uint32
 	docWriteFailures           uint32
@@ -12,100 +14,73 @@ type ReplicationStats struct {
 	attachmentBytesTransferred uint64
 	docsCheckedSent            uint64
 	endLastSeq                 interface{}
+	endLastSeqLock             sync.RWMutex
 }
 
 func (rs *ReplicationStats) GetNumAttachmentsTransferred() uint64 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.numAttachmentsTransferred
+	return atomic.LoadUint64(&rs.numAttachmentsTransferred)
 }
 
 func (rs *ReplicationStats) AddNumAttachmentsTransferred(delta uint64) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.numAttachmentsTransferred += delta
+	atomic.AddUint64(&rs.numAttachmentsTransferred, delta)
 }
 
 func (rs *ReplicationStats) GetAttachmentBytesTransferred() uint64 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.attachmentBytesTransferred
+	return atomic.LoadUint64(&rs.attachmentBytesTransferred)
 }
 
 func (rs *ReplicationStats) AddAttachmentBytesTransferred(delta uint64) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.attachmentBytesTransferred += delta
+	atomic.AddUint64(&rs.attachmentBytesTransferred, delta)
 }
 
 func (rs *ReplicationStats) GetDocsCheckedSent() uint64 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.docsCheckedSent
+	return atomic.LoadUint64(&rs.docsCheckedSent)
 }
 
 func (rs *ReplicationStats) AddDocsCheckedSent(delta uint64) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.docsCheckedSent += delta
+	atomic.AddUint64(&rs.docsCheckedSent, delta)
 }
 
 func (rs *ReplicationStats) GetDocsRead() uint32 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.docsRead
+	return atomic.LoadUint32(&rs.docsRead)
 }
 
 func (rs *ReplicationStats) AddDocsRead(addDocsRead uint32) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.docsRead += addDocsRead
+	atomic.AddUint32(&rs.docsRead, addDocsRead)
 }
 
 func (rs *ReplicationStats) GetDocsWritten() uint32 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.docsWritten
+	return atomic.LoadUint32(&rs.docsWritten)
 }
 
 func (rs *ReplicationStats) AddDocsWritten(addDocsWritten uint32) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.docsWritten += addDocsWritten
+	atomic.AddUint32(&rs.docsWritten, addDocsWritten)
 }
 
 func (rs *ReplicationStats) GetDocWriteFailures() uint32 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.docWriteFailures
+	return atomic.LoadUint32(&rs.docWriteFailures)
 }
 
 func (rs *ReplicationStats) AddDocWriteFailures(addDocWriteFailures uint32) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.docWriteFailures += addDocWriteFailures
+	atomic.AddUint32(&rs.docWriteFailures, addDocWriteFailures)
 }
 
 func (rs *ReplicationStats) GetStartLastSeq() uint32 {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
-	return rs.startLastSeq
+	return atomic.LoadUint32(&rs.startLastSeq)
 }
 
 func (rs *ReplicationStats) SetStartLastSeq(updatedStartLastSeq uint32) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
-	rs.startLastSeq = updatedStartLastSeq
+	atomic.StoreUint32(&rs.startLastSeq, updatedStartLastSeq)
 }
 
 func (rs *ReplicationStats) GetEndLastSeq() interface{} {
-	rs.lock.RLock()
-	defer rs.lock.RUnlock()
+	rs.endLastSeqLock.RLock()
+	defer rs.endLastSeqLock.RUnlock()
 	return rs.endLastSeq
 }
 
 func (rs *ReplicationStats) SetEndLastSeq(updatedEndLastSeq interface{}) {
-	rs.lock.Lock()
-	defer rs.lock.Unlock()
+	rs.endLastSeqLock.Lock()
+	defer rs.endLastSeqLock.Unlock()
 	rs.endLastSeq = updatedEndLastSeq
 }
